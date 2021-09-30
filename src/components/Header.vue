2629bf2aef4708953671ed1919453e4cb582d3d3
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-30 23:57:54 +0200

    header init

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <!-- <img src="img/spoti-logo.png" alt=""> -->
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+@import '../general/variables.scss';
+
+.row{
+    height: 75px;
+    background-color:$primaryGrey;
+}
+
+</style>
