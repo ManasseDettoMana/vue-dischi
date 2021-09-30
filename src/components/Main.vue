cd9aa201618cd4a06e014df2917cb4698cb24c01
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-30 23:56:37 +0200

    main init

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class=""></div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss">
+@import '../general/variables.scss';
+main{
+    background-color: #2b2b3d;
+    height: calc(100vh - 75px);
+}
+
+</style>
